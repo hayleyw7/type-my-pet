2de209b99d2694554eb883ae758147d0a3e41b0e
Hayley Witherell <hayleywitherell@gmail.com>
2021-12-01 16:59:33 -0700

    Create App skeleton

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,23 @@
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Type My Pet</h1>
-      </header>
-    </div>
-  );
+import './App.css';
+// import X from '../X/X';
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      type: []
+    }
+  }
+
+  render() {  
+    return (
+      <div className="App">
+        <h1>Type My Pet</h1>     
+      </div>
+    );
+  }
 }
 
 export default App;
